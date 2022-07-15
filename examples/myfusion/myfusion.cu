b2f405b22a492de87baf5191d2954992dd8153b8
umiswing <umiswing@foxmail.com>
2022-07-15 19:58:25 +0800

    add myfusion to examples

--- examples/myfusion/myfusion.cu
+++ examples/myfusion/myfusion.cu
@@ -59,11 +59,11 @@ using SmArch = cutlass::arch::Sm80;
 
 // This code section describes the tile size a thread block will compute
 using ShapeMMAThreadBlock =
-    cutlass::gemm::GemmShape<128, 128, 4>;  // <- threadblock tile M = 128, N = 128, K = 32
+    cutlass::gemm::GemmShape<128, 128, 16>;  // <- threadblock tile M = 128, N = 128, K = 32
 // This code section describes tile size a warp will compute
-using ShapeMMAWarp = cutlass::gemm::GemmShape<64, 64, 4>;  // <- warp tile M = 64, N = 64, K = 32 
+using ShapeMMAWarp = cutlass::gemm::GemmShape<64, 64, 16>;  // <- warp tile M = 64, N = 64, K = 32 
 // This code section describes the size of MMA op
-using ShapeMMAOp = cutlass::gemm::GemmShape<16, 8, 16>;  // <- MMA Op tile M = 8, N = 8, K = 4
+using ShapeMMAOp = cutlass::gemm::GemmShape<16, 8, 8>;  // <- MMA Op tile M = 8, N = 8, K = 4
 // 16, 8, 8 -> Turing
 // 16, 8, 16 -> Ampere
 
@@ -76,7 +76,8 @@ using SwizzleThreadBlock = cutlass::gemm::threadblock::GemmIdentityThreadblockSw
 //
 using EpilogueOp = cutlass::epilogue::thread::LinearCombination<
     ElementOutput,                                        // <- data type of output matrix
-    128 / cutlass::sizeof_bits<ElementOutput>::value,     // <- this is the number of elements per
+    //128 / cutlass::sizeof_bits<ElementOutput>::value,     // <- this is the number of elements per
+    1,
                                                           // vectorized memory access. For half
                                                           // precision, it's 8 elements. This becomes
                                                           // the vector width of math instructions in
@@ -104,14 +105,14 @@ using Gemm = cutlass::gemm::device::GemmUniversal<ElementInputA,
                                                   EpilogueOp,
                                                   SwizzleThreadBlock,
                                                   NumStages,
-                                                  8,     /*alignmentA*/
-                                                  8,     /*alignmengB*/
+                                                  1,     /*alignmentA*/
+                                                  1,     /*alignmengB*/
                                                   cutlass::arch::OpMultiplyAdd,
                                                   cutlass::ComplexTransform::kNone,
                                                   cutlass::ComplexTransform::kNone,
-                                                  true,  /*GatherA*/
-                                                  true,  /*GatherB*/
-                                                  true   /*ScatterD*/
+                                                  false,  /*GatherA*/
+                                                  false,  /*GatherB*/
+                                                  false   /*ScatterD*/
                                                  >;
 
 int main() {
@@ -119,13 +120,15 @@ int main() {
   int k=2;
   int n=4;
 
-  std::vector<std::vector<float>> a = {{1, 1}, {0, 0}, {1, 1}, {0, 0}};
-  std::vector<std::vector<float>> b = {{2}, {3}, {4}, {5}};
-  std::vector<float> d = {0, 0, 0, 0};
+  //std::vector<std::vector<float>> a = {{1, 1}, {0, 0}, {1, 2}, {0, 0}};
+  //std::vector<std::vector<float>> b = {{2, 6, 3, 7}, {4, 8, 5, 9}};
+  std::vector<std::vector<float>> a = {{1, 1}, {1, 1}, {1, 1}, {1, 1}};
+  std::vector<std::vector<float>> b = {{1, 1, 1, 1}, {1, 1, 1, 1}};
+  std::vector<float> d = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 
-  std::vector<int> a_idx = {0, 2};
-  std::vector<int> b_idx = {1, 2};
-  std::vector<int> d_idx = {0, 3};
+  std::vector<int> a_idx = {0,1};
+  std::vector<int> b_idx = {0,1};
+  std::vector<int> d_idx = {0,1};
 
   std::vector<float> a_flattened = flatten(a);
   std::vector<float> b_flattened = flatten(b);
@@ -134,46 +137,46 @@ int main() {
 
   cutlass::gemm::GemmCoord problem_size(m, n, k);
   cutlass::gemm::GemmCoord problem_size_real(problem_size.m(),
-                                             index_size,
-                                             index_size);
+                                             problem_size.n(),
+                                             problem_size.k());
   float *d_a;
   float *d_b;
   float *d_c;
   float *d_d;
 
-  cudaMalloc(&d_a, 8 * sizeof(float));
-  cudaMalloc(&d_b, 4 * sizeof(float));
-  cudaMalloc(&d_c, 4 * sizeof(float));
-  cudaMalloc(&d_d, 4 * sizeof(float));
+  cudaMalloc(&d_a, m * k * sizeof(float));
+  cudaMalloc(&d_b, m * k * sizeof(float));
+  cudaMalloc(&d_c, m * n * sizeof(float));
+  cudaMalloc(&d_d, m * n * sizeof(float));
+  cudaDeviceSynchronize();
 
-  cudaMemcpy(d_a, a_flattened.data(), 8 * sizeof(float),
+  cudaMemcpy(d_a, a_flattened.data(), m * k * sizeof(float),
              cudaMemcpyHostToDevice);
-  cudaMemcpy(d_b, b_flattened.data(), 4 * sizeof(float),
+  cudaMemcpy(d_b, b_flattened.data(), m * k * sizeof(float),
              cudaMemcpyHostToDevice);
-  cudaMemset(d_c, 0, 4 * sizeof(float));
-  cudaMemcpy(d_d, d.data(), 4 * sizeof(float), cudaMemcpyHostToDevice);
+  cudaMemset(d_c, 0, m * n * sizeof(float));
+  cudaMemcpy(d_d, d.data(), m * n * sizeof(float), cudaMemcpyHostToDevice);
 
   int *d_a_idx;
   int *d_b_idx;
   int *d_d_idx;
 
-  cudaMalloc(&d_a_idx, 2 * sizeof(int));
-  cudaMalloc(&d_b_idx, 2 * sizeof(int));
-  cudaMalloc(&d_d_idx, 2 * sizeof(int));
+  cudaMalloc(&d_a_idx, a_idx.size() * sizeof(int));
+  cudaMalloc(&d_b_idx, b_idx.size() * sizeof(int));
+  cudaMalloc(&d_d_idx, d_idx.size() * sizeof(int));
+  cudaDeviceSynchronize();
 
-  cudaMemcpy(d_a_idx, a_idx.data(), 2 * sizeof(int), cudaMemcpyHostToDevice);
-  cudaMemcpy(d_b_idx, b_idx.data(), 2 * sizeof(int), cudaMemcpyHostToDevice);
-  cudaMemcpy(d_d_idx, d_idx.data(), 2 * sizeof(int), cudaMemcpyHostToDevice);
+  cudaMemcpy(d_a_idx, a_idx.data(), a_idx.size() * sizeof(int), cudaMemcpyHostToDevice);
+  cudaMemcpy(d_b_idx, b_idx.data(), b_idx.size() * sizeof(int), cudaMemcpyHostToDevice);
+  cudaMemcpy(d_d_idx, d_idx.data(), d_idx.size() * sizeof(int), cudaMemcpyHostToDevice);
+  cudaDeviceSynchronize();
   ElementComputeEpilogue alpha = ElementComputeEpilogue(1);
   ElementComputeEpilogue beta = ElementComputeEpilogue(1);
 
   int split_k_slices = 1;
-  cutlass::layout::RowMajor().capacity(problem_size.mk());
-  Gemm::Arguments;
-#if 0
   typename Gemm::Arguments arguments{
       cutlass::gemm::GemmUniversalMode::kGemm,
-      problem_size_real, // <- problem size of matrix multiplication
+      problem_size, // <- problem size of matrix multiplication
       split_k_slices,    // <- k-dimension split factor
       {alpha, beta},     // <- alpha, beta
       d_a,               // <- reference to matrix A on device
@@ -181,17 +184,16 @@ int main() {
       d_c,               // <- reference to matrix C on device
       d_d,               // <- reference to matrix D on device
       cutlass::layout::RowMajor().capacity(problem_size.mk()),
-      cutlass::layout::RowMajor().capacity(
-          cutlass::make_Coord(index_size, problem_size.n())),
+      cutlass::layout::RowMajor().capacity(problem_size.kn()),
       cutlass::layout::RowMajor().capacity(problem_size.mn()),
       cutlass::layout::RowMajor().capacity(problem_size.mn()),
       cutlass::layout::RowMajor().stride(),
       cutlass::layout::RowMajor().stride(),
       cutlass::layout::RowMajor().stride(),
       cutlass::layout::RowMajor().stride(),
-      d_a_idx,  // <- pointer to index vector to gather A on device
-      d_b_idx,  // <- pointer to index vector to gather B on device
-      d_d_idx}; // <- pointer to index vector to scatter D on device
+      nullptr,  // <- pointer to index vector to gather A on device
+      nullptr,  // <- pointer to index vector to gather B on device
+      nullptr}; // <- pointer to index vector to scatter D on device
 
   // Using the arguments, query for extra workspace required for matrix multiplication computation
   size_t workspace_size = Gemm::get_workspace_size(arguments);
@@ -203,22 +205,21 @@ int main() {
   Gemm gemm_op;
 
   // Check the problem size is supported or not
-  //cutlass::Status status = gemm_op.can_implement(arguments);
-  cutlass::Status status;
+  cutlass::Status status = gemm_op.can_implement(arguments);
   CUTLASS_CHECK(status);
 
   // Initialize CUTLASS kernel with arguments and workspace pointer
-  //status = gemm_op.initialize(arguments, workspace.get());
+  status = gemm_op.initialize(arguments, workspace.get());
   CUTLASS_CHECK(status);
 
-  //status = gemm_op();
+  status = gemm_op();
   cudaDeviceSynchronize();
   CUTLASS_CHECK(status);
 
-  cudaMemcpy(d.data(), d_d, 4 * sizeof(float), cudaMemcpyDeviceToHost);
+  cudaMemcpy(d.data(), d_d, m*n * sizeof(float), cudaMemcpyDeviceToHost);
+  cudaDeviceSynchronize();
 
-  for (int i = 0; i < 4; i++) {
+  for (int i = 0; i < m*n; i++) {
     std::cout << d.at(i) << ',';
   }
-#endif
 }
